82c5f33b5a8261ceb05d8a2505159d37a5441528
itawenya <93374574+itawenya@users.noreply.github.com>
2023-09-03 12:07:49 +0800

    feat:overview
    1.overview页面的初步布局
    2.添加data文件夹用来存储table等需要用到的配置项信息

--- src/pages/overview.vue
+++ src/pages/overview.vue
@@ -1,88 +1,156 @@
 <template>
-  <div>
-      总览
-      <button @click="getNumberOfMachine">获取1</button>
-      <button @click="getMachineStatusById">获取2</button>
-      <button @click="getMachineStatus">获取3</button>
-
-      <button @click="getKey">Key</button>
-      <button @click="login">login</button>
+  <div id="container" class="center">
+    <div id="databox">
+      <p>刷新频率：</p>
+      <el-select
+        style="width: 110px"
+        v-model="selectedValue"
+        class="m-2"
+        placeholder="选择频率"
+      >
+        <el-option
+          v-for="item in options"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value"
+        />
+      </el-select>
+      <el-button>手动刷新数据</el-button>
+    </div>
+    <el-row :gutter="30">
+      <el-col :span="6">
+        <div class="ep-bg-purple short" id="hourse">
+          <h2>房间</h2>
+          <el-icon class="icon"><House /></el-icon>
+        </div>
+        <div class="ep-bg-purple short" id="number">
+          <h2>空调内机</h2>
+          <el-icon class="icon"><CreditCard /></el-icon>
+        </div>
+      </el-col>
+      <el-col :span="12"
+        ><div class="grid-content ep-bg-purple" id="data">
+          <h2>中央空调内机运行信息</h2>
+          <el-icon class="icon"><Cpu /></el-icon>
+          <el-table class="center" :data="tableData" style="width: 95%" height="420">
+            <el-table-column prop="date" label="房间" width="120" />
+            <el-table-column prop="name" label="内机" width="100" />
+            <el-table-column prop="state" label="开/关" width="100" />
+            <el-table-column prop="city" label="模式" width="100" />
+            <el-table-column prop="address" label="风速" width="100" />
+            <el-table-column prop="zip" label="温度" width="120" />
+            <el-table-column prop="zip" label="室温" width="120" />
+            <el-table-column prop="zip" label="状态" width="120" />
+          </el-table></div
+      ></el-col>
+      <el-col :span="6"
+        ><div class="grid-content ep-bg-purple" id="warn">
+          <h2>报警信息</h2>
+          <el-icon class="icon"><WarnTriangleFilled /></el-icon></div
+      ></el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
-import { post, get } from '../utils/http.js'
-import { encryptedData } from "../utils/encrypt";
-export default{
-  name:'Overview',
+import { post, get } from "../utils/http.js";
+import { ref } from "vue";
+import reloadTime from "../data/overview/reloadTime";
+import machineStatus from "../data/overview/machineStatus"
+
+export default {
+  name: "Overview",
   setup() {
-    const getNumberOfMachine = async ()=> {
-      const res = await post('/getNumberOfMachine',{
-        id: "16_201"
-      })
-      console.log("查询设备下的内机数量:",res);
-    }
-    const getMachineStatusById = async ()=> {
-      const res = await post('/getMachineStatusById',{
-        id: "16"
-      })
-
-      console.log("通过房间号或楼栋号内机状态:",res);
-    }
-    const getMachineStatus = async ()=> {
-      const res = await post('/getMachineStatus',{
-        "name":"16_201_1"
-      })
-
-      console.log("查询指定内机状态:",res);
-    }
-    const controlMachine = async ()=> {
-      const res = await post('/controlMachine',
-      {
-          "name":"16", 
-          "status": 0,
-          "mode": 1,
-          "temperature": 19,
-          "windSpeed": 1
-      }
-      )
-      console.log("控制接口:",res);
-    }
-
-    /* 登录测试 */
-
-    let publicKey = null
-    let password = 123456
-    let afterPwd = 123456
-    // 获取公钥：
-    const getKey = async ()=>{
-      const res = await get('/login')
-      publicKey = res
-      console.log("公钥:",publicKey);
-      const encryptedPassword = encryptedData(publicKey.data, password);
-
-      console.log(encryptedPassword);
-      afterPwd = encryptedPassword
-    }
-
-    // 用密钥登录
-    const login = async ()=> {
-      console.log(afterPwd);
-      const res = await post('/login',{
-        "username": "12928",
-        "password": afterPwd
-      })
-      console.log("登录情况:",res);
-    }
+    const selectedValue = ref("");
     return {
-      getNumberOfMachine,
-      getMachineStatusById,
-      getMachineStatus,
-      controlMachine,
-      getKey,
-      login
-    }
+      selectedValue,
+      options: reloadTime.options,
+      tableData: machineStatus.tableData
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+#container {
+  width: 90%;
+}
+
+.center{
+  left: 50%;
+  transform: translateX(-50%);
+  position: relative;
+}
+
+#databox {
+  position: relative;
+  top: 20px;
+  display: flex;
+  justify-items: center;
+  height: 80px;
+  p {
+    line-height: 30px;
+  }
+  button {
+    margin-left: 15px;
   }
 }
 
-</script>
+.grid-content {
+  height: 520px;
+  position: relative;
+}
+
+.short {
+  height: 250px;
+  position: relative;
+}
+.ep-bg-purple.short:nth-child(2) {
+  /* 添加样式，例如修改背景颜色 */
+  margin-top: 20px;
+}
+.ep-bg-purple {
+  overflow: hidden;
+  transition: all 0.3s;
+  cursor: pointer;
+  border-radius: 4px;
+  background-color: rgb(231,238,243);
+  h2 {
+    display: block;
+    margin-top: 20px;
+  }
+  .icon {
+    position: absolute;
+    font-size: 65px;
+    bottom: 5px;
+    right: 25px;
+    opacity: 0.2;
+  }
+}
+.ep-bg-purple:hover {
+  transform: scale(1.02);
+}
+/*最外层透明*/
+::v-deep .el-table,
+::v-deep .el-table__expanded-cell {
+ background-color: transparent !important;
+}
+/* 表格内背景颜色 */
+::v-deep .el-table th,
+::v-deep .el-table tr,
+::v-deep .el-table td {
+ background-color: transparent !important;
+ border: 0; //去除表格
+}
+/*去除底边框*/
+::v-deep.el-table td.el-table__cell {
+ border: 0;
+}
+::v-deep.el-table th.el-table__cell.is-leaf {
+ border: 0;
+}
+/*去除底部灰条.el-table::before*/
+::v-deep .el-table__inner-wrapper::before {
+  display: none;
+}
+</style>
